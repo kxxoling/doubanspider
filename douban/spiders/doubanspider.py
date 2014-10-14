c2b5f511f36c813722b6c08c4a295acc1c3e4db5
kxxoling <kxxoling@gmail.com>
2014-10-14 09:08:59 +0800

    数据改以列表的形式整个存储下来

--- douban/spiders/doubanspider.py
+++ douban/spiders/doubanspider.py
@@ -7,30 +7,23 @@ from douban.items import DoubanItem
 class DoubanSpider(Spider):
     name = 'douban'
     allowed_domains = ['movie.douban.com']
-    start_urls = [#zxd 'http://movie.douban.com/tag/%E5%8A%A8%E6%BC%AB/',
-                'http://movie.douban.com/subject/1439831/']
+    start_urls = ['http://movie.douban.com/subject/1439831/']
+# zxd 'http://movie.douban.com/tag/%E5%8A%A8%E6%BC%AB/',
 
     def parse(self, response):
         selector = html.fromstring(response.body)
 
         item = DoubanItem()
-        item['name'] = selector.xpath('//span[@property="v:itemreviewed"]/text()')[0]
-        item['director'] = [i.text
-                            for i in selector.xpath(u'//div[@id="info"]//span[text()="导演"]/following-sibling::a')]
-        item['dramatist'] = [i.text
-                             for i in selector.xpath(u'//div[@id="info"]//span[text()="编剧"]/following-sibling::a')]
-        item['actors'] = [i.text
-                          for i in selector.xpath(u'//div[@id="info"]//span[text()="主演"]/following-sibling::a')]
-        item['tags'] = [i.text for i in selector.xpath(u'//div[@id="info"]//span[text()="类型:"]'
-                                                       u'/following-sibling::span[@property="v:genre"]')]
-        item['official_website'] = selector.xpath(u'//div[@id="info"]//span[text()="官方网站:"]'
-                                                  u'/following-sibling::a/@href')[0]
-        # item['area'] = selector.xpath('')
-        # item['language'] = selector.xpath('')
-        # item['premiere'] = selector.xpath('')
-        # item['episodes'] = selector.xpath('')
-        # item['aka'] = selector.xpath('')
-        # item['description'] = selector.xpath('')
-        # item['imdb'] = selector.xpath('')
+        info_div = selector.xpath('//div[@id="info"]')[0]
+        info_list = []
+        for info_item in info_div.text_content().split('\n'):
+            info_list.append(map(lambda x: x.strip()), info_item.split(': '))
 
-        return item
+        item['name'] = selector.xpath('//span[@property="v:itemreviewed"]/text()')[0]
+        item['director'] = [i.text for i in selector.xpath(u'//div[@id="info"]//span[text()="导演"]/following-sibling::a')]
+        item['dramatist'] = [i.text for i in selector.xpath(u'//div[@id="info"]//span[text()="编剧"]/following-sibling::a')]
+        item['actors'] = [i.text for i in selector.xpath(u'//div[@id="info"]//span[text()="主演"]/following-sibling::a')]
+        item['tags'] = [i.text for i in selector.xpath(u'//div[@id="info"]//span[text()="类型:"]/following-sibling::span[@property="v:genre"]')]
+        item['official_website'] = selector.xpath(u'//div[@id="info"]//span[text()="官方网站:"]/following-sibling::a/@href')[0]
+        item
+        return info_list
